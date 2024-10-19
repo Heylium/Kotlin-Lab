ee2581557b56c152148159a45b97e5b022231395
Heylium <tuanhsiang@163.com>
2024-10-19 23:37:42 +0800

    Calculator v1.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,41 @@
 package org.example
 
+import kotlin.system.exitProcess
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    while (true) {
+        println("请输入标准的算式，并且按回车; \n" + "比如：1 + 1，注意符合与数字之间要有空格。\n" + "输入exit，退出程序。")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+        var input = readLine()
+        if (input == null) continue
+        if (input == "exit") exitProcess(0)
+
+        var inputList = input.split(" ")
+        var result = calculate(inputList)
+
+        if (result == null) {
+            println("输入格式不对")
+            continue
+        } else {
+            println("$input = $result")
+        }
+    }
+}
+
+private fun calculate(inputList: List<String>): Int? {
+    if (inputList.size != 3) return null
+
+    var left = inputList.get(0).toInt()
+    var operation = inputList.get(1)
+    var right = inputList.get(2).toInt()
+
+    when(operation) {
+        "+" -> return left + right
+        "-" -> return left - right
+        "*" -> return left * right
+        "/" -> return left / right
+        else -> return null
     }
 }
